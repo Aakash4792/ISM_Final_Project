dcfdf25c10a314b08fb63749cf8e60aa2e142794
Aakash Sivakumar <aakashsivakumar02@gmail.com>
2023-04-04 10:07:47 +0530

    initial commit

---
+++ migrations/1_initial_migration.js
@@ -0,0 +1,13 @@
+const Migrations = artifacts.require("Migrations");
+const DaiTokenMock = artifacts.require("DaiTokenMock");
+
+module.exports = async function(deployer) {
+  await deployer.deploy(Migrations);
+  await deployer.deploy(DaiTokenMock);
+  const tokenMock = await DaiTokenMock.deployed();
+  // Mint 1,000 Dai Tokens for the deployer
+  await tokenMock.mint(
+    "0x0a5F5503EAcacCFB622c00847157fEd3BB4Adf9b",
+    "1000000000000000000000"
+  );
+};
